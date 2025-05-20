a3cb6ec4d0d1b8bedb1d09f952e6c995240fbe5f
Maksym <80504073+gorshok-py@users.noreply.github.com>
2025-05-20 11:18:16 +0300

    Update traffic_checker.py
    Add a summary counter for download and upload.

--- traffic_checker.py
+++ traffic_checker.py
@@ -20,26 +20,30 @@ class log_interface(log_parse):
         return all_logins
     
     def parse_speed(self, logins, lines_from_parse_log):
+        sum_download = 0
+        sum_upload = 0
         for names in logins: #парсимо логіни
             total_download = 0
             total_upload = 0
             counter = 0
             for lines in lines_from_parse_log: #Парсимо весь лог
-                if names in lines[2]: #якщо логін є у лінії тоді робимо наступне
+                if names in lines[2]: #якщо логін є у лініє тоді робимо наступне
                     counter += 1
                     download = int(lines[5])
                     upload = int(lines[6])
                     total_download += download
                     total_upload += upload
+                    sum_download += download
+                    sum_upload += upload
 
-
-
-            print(f"Кількість спожитого трафіку для користувача. Логін: {names}\n{total_download / 1000000} Мегабайт завантажено\n{total_upload / 1000000} Мегабайт вивантажено \nСумарна кількість запитів від користувача {counter}")
-
-
-        
+            print(f"Логін: {names}"
+                  f"\nМегабайт завантажено:\n{total_download / 1000000} MB"
+                  f"\nМегабайт вивантажено:\n{total_upload / 1000000} MB"
+                  f"\nСумарна кількість запитів від користувача {counter}")
             
-
+        print(f"Усього трафіку спожито в лог файлі: {sys.argv[1]}\n"
+              f"Завантажено: {sum_download / 1000000} MB"
+              f"\nВивантажено: {sum_upload / 1000000} MB")
 
 
 logs = log_interface()
